f8e18d1908521c71d99d139a241e602549e6e313
vyybui <vkb2013@nyu.edu>
2026-02-11 00:09:51 +0400

    script

---
+++ albertmovie/script.js
@@ -0,0 +1,7 @@
+let joinclick = document.getElementById("joinus");
+
+if (joinclick) {
+    joinclick.addEventListener("click", () => {
+        alert("APPLICATION UNSUCCESSFUL. You have been waitlisted. You are: 16th on the waitlist :) ");
+    });
+}
